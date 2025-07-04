e63faa8584deae9552e249c338051688a03ad63d
Kevin From <kf+bitbucket@vestjyskmarketing.dk>
2025-07-04 11:34:16 +0200

    codestyle!

--- app/Application.php
+++ app/Application.php
@@ -11,13 +11,14 @@ use Core\BaseApplication;
 use Core\Config\ConfigInterface;
 use Core\Http\Middleware\Queue\MiddlewareQueueInterface;
 use Core\Http\Router\RouterInterface;
+use Override;
 
 use function Core\redirect;
 use function Core\view;
 
 final class Application extends BaseApplication
 {
-    #[\Override]
+    #[Override]
     public function configuration(ConfigInterface $config): ConfigInterface
     {
         $config = parent::configuration($config);
@@ -29,7 +30,7 @@ final class Application extends BaseApplication
     /**
      * @inheritDoc
      */
-    #[\Override]
+    #[Override]
     public function middleware(MiddlewareQueueInterface $middleware): void
     {
         parent::middleware($middleware);
